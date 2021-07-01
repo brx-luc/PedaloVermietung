2e37ac408883d0fd6dd598b890bc7bff8ba3e3a1
luca <luca.bravo11@gmail.com>
2021-07-01 11:16:24 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/PedaloController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedalo")
+public class PedaloController {
+
+    private final PedaloRepository pedaloRepository;
+
+    public PedaloController(PedaloRepository pedaloRepository){
+        this.pedaloRepository = pedaloRepository;
+    }
+
+    @GetMapping
+    public List<Pedalo> getPedalo(){
+        return pedaloRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Pedalo getPedalo(@PathVariable Long id){
+        return pedaloRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createPedalo(@RequestBody Pedalo pedalo) throws URISyntaxException {
+        Pedalo savedPedalo = pedaloRepository.save(pedalo);
+        return ResponseEntity.created(new URI("/pedalo/" + savedPedalo.getId())).body(savedPedalo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updatePedalo(@PathVariable Long id, @RequestBody Pedalo pedalo){
+        Pedalo currentPedalo = pedaloRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentPedalo.setName(pedalo.getName());
+        currentPedalo = pedaloRepository.save(currentPedalo);
+
+        return ResponseEntity.ok(currentPedalo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletePedalo(@PathVariable Long id) {
+        pedaloRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
